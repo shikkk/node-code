f1273c548821919604c99eb0651f4e84f0c31236
shizhikai <shizhikai@cmcm.com>
2019-11-15 15:43:05 +0800

    添加redis-test

---
+++ redis-test/index.js
@@ -0,0 +1,24 @@
+const redis = require('redis')
+
+//创建客户端
+
+const redisClient = redis.createClient(6379,'127.0.0.1')
+redisClient.on('error',err => {
+    console.log(err)
+})
+
+//测试
+
+redisClient.set('myname','lisi',redis.print)
+
+redisClient.get('myname', (err,value) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(value)
+
+    //退出redis
+    redisClient.quit()
+})
+
