ba88fa24a246b873470323cbdb00a30d843102b6
shizhikai <shizhikai@cmcm.com>
2019-11-15 15:48:47 +0800

    设置cookie

--- blog/src/router/user.js
+++ blog/src/router/user.js
@@ -2,14 +2,24 @@
 const {loginCheck}= require('../controller/user')
 const {successModel, errorModel} = require('../model/resModel')
 
+//设置cookie过期时间
+const getCookieExpires = () => {
+    let d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
+
 const userRouter = (req,res) => {
     const method = req.method;
     //登录
-    if(method === 'POST' && req.path === '/api/user/login'){
-        const {username,password} = req.body
+    if(method === 'GET' && req.path === '/api/user/login'){
+        // const {username,password} = req.body
+        const {username,password} = req.query
         const result = loginCheck(username,password)
         return result.then(loginData => {
             if(loginData.username){
+                //httpOnly cookie 只允许后台修改cookie
+                res.setHeader('Set-Cookie',`username=${loginData.username};path=/;httpOnly;expires=${getCookieExpires()}`)
                 return new successModel({},'登录成功！')
              }else{
                  return new errorModel(null,'登录失败！')
