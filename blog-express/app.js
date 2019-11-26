50f947b835697c4527c6120ee4d20607c669b8b0
shizhikai <shizhikai@cmcm.com>
2019-11-26 14:24:17 +0800

    express日志

--- blog-express/app.js
+++ blog-express/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var fs = require('fs')
 var path = require('path');
 var cookieParser = require('cookie-parser'); // cookie插件
 var logger = require('morgan');   // 日志插件
@@ -16,7 +17,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+//日志
+if(process.env.NODE_ENV !== 'production'){
+  //开发环境
+  app.use(logger('dev'));
+}else{
+  //线上环境
+  const logFileName = path.join(__dirname,'logs','access.log')
+  const writeStream = fs.createWriteStream(logFileName,{
+    flags:'a'
+  })
+  app.use(logger('combined',{
+    stream:writeStream
+  }));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
