8cceb23978a75fa09c45f5db2816fb94cda3aaaf
shizhikai <shizhikai@cmcm.com>
2019-12-04 10:14:05 +0800

    async/await

--- promise-test/index.js
+++ promise-test/index.js
@@ -18,13 +18,32 @@ function getFileContent(fileName){
     })
     return promise
 }
+// promise 获取内容
+// getFileContent('a.json').then(adata => {
+//     console.log('a',adata.next)
+//     return getFileContent(adata.next)
+// }).then(bdata => {
+//     console.log('b',bdata.next)
+//     return getFileContent(bdata.next)
+// }).then(cdata => {
+//     console.log('c',cdata.next)
+// })
 
-getFileContent('a.json').then(adata => {
-    console.log('a',adata.next)
-    return getFileContent(adata.next)
-}).then(bdata => {
-    console.log('b',bdata.next)
-    return getFileContent(bdata.next)
-}).then(cdata => {
-    console.log('c',cdata.next)
-})
+// async/await  获取内容
+// 1、await 后面可以追加promise对象，获取resolve的值
+// 2、await 必须包裹在async函数里
+// 3、async函数执行返回的也是一个promise对象
+// 4、try-catch截获promise中reject的值
+async function readFileData(){
+    try {
+        const a = await getFileContent('a.json')
+        console.log(a)
+        const b = await getFileContent(a.next)
+        console.log(b)
+        const c = await getFileContent(b.next)
+        console.log(c)
+    } catch (error) {
+        console.log(error)
+    }
+}
+readFileData()
